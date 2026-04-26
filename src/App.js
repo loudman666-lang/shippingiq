f071b4e3369495b3593771632c5b30c134215155
loudman666-lang <loudman666@gmail.com>
2026-04-27 09:45:04 +1000

    Build Carriers page with AI file parsing

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
-import { ProtectedRoute, AdminRoute } from './components/auth/ProtectedRoute'
+import { ProtectedRoute } from './components/auth/ProtectedRoute'
 
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import ForgotPassword from './pages/ForgotPassword'
 import ResetPassword from './pages/ResetPassword'
 import Dashboard from './pages/Dashboard'
+import Carriers from './pages/Carriers'
 
 export default function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          {/* Public routes */}
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password" element={<ResetPassword />} />
-
-          {/* Protected routes */}
-          <Route path="/dashboard" element={
-            <ProtectedRoute><Dashboard /></ProtectedRoute>
-          } />
-
-          {/* Redirect root to dashboard */}
+          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/carriers" element={<ProtectedRoute><Carriers /></ProtectedRoute>} />
           <Route path="/" element={<Navigate to="/dashboard" replace />} />
-
-          {/* 404 fallback */}
           <Route path="*" element={<Navigate to="/dashboard" replace />} />
         </Routes>
       </AuthProvider>
